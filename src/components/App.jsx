de7975d84de99b60f7c7ce38f93e2d53b102b8a5
Liubov <drogaltseva92@gmail.com>
2023-12-31 14:59:28 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,13 +11,13 @@ import ContactList from "./Form/ContacsList";
 export class App extends Component {
 state = {
   contacts: [
-    // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    // {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    // {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    // {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
 ],
-  name: '',
-  number: '',
+  // name: '',
+  // number: '',
   filter: '',
   }
   
@@ -39,13 +39,13 @@ state = {
       contacts: [
         ...this.state.contacts,
         {
-          number: this.state.number,
-          name: this.state.name,
+          // number: this.state.number,
+          // name: this.state.name,
           id: nanoid(),
         }
       ],
-      name: '',
-      number: ''
+      // name: '',
+      // number: ''
     });
     event.currentTarget.reset()
   }
